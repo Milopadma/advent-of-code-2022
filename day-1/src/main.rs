40ac06dfe40f974aebe13d6df900de2d60d0e687
mylo <milopadma@yahoo.com>
2022-12-01 16:36:50 +0800

    progress

--- day-1/src/main.rs
+++ day-1/src/main.rs
@@ -10,7 +10,7 @@ fn main() {
         input_split_per_number.push(i.split("\n").collect::<Vec<&str>>());
     }
     // turn every string that is a number into an integer, leave the empty strings alone
-    let mut input_integers = Vec::new();
+    let mut input_integers = Vec::new(); //new vector array
     for i in &input_split_per_number {
         let mut temp = Vec::new();
         for j in i {
@@ -21,7 +21,7 @@ fn main() {
         input_integers.push(temp);
     }
     // for every array in the array, add it to a new array and create a new array everytime an empty array is found
-    let mut input_integers_2 = Vec::new();
+    let mut input_integers_2 = Vec::new(); //new vector array
     let mut temp = Vec::new();
     for i in &input_integers {
         if i.len() == 0 {
@@ -32,5 +32,11 @@ fn main() {
         }
     }
     input_integers_2.push(temp);
-    println!("{:?}", input_integers_2);
+
+    // for every array in inputintegers2, pair it with a key of "elf-number"
+    let mut input_integers_3 = Vec::new(); //new vector array
+    for i in 1..input_integers_2.len() {
+        input_integers_3.push(("elf-".to_string() + &i.to_string(), input_integers_2[i].clone()));
+    }
+    println!("{:?}", input_integers_3);
 }
