2d123409058c81190533f8ad215a439b1e08bf00
mylo <milopadma@yahoo.com>
2022-12-01 16:45:25 +0800

    helpful error messages

--- day-1/src/main.rs
+++ day-1/src/main.rs
@@ -40,15 +40,22 @@ fn main() {
     }
     // println!("{:?}", input_integers_3);
 
-    // now time to find the elf with the most calories
-    let mut mostcalories = findMostCalories(input_integers_3);
+    // now time to find the elf with the most calories, for part 1
+    let mostcalories: (String, i32) = find_most_calories(&input_integers_3);
     println!("The elf with the most calories is: {:?}", mostcalories);
+
+    // for part 2, finding the total calories of the top three elves
+    let top3calories = top_three_calorie_elfs(&input_integers_3);
+    // to add the calories together, we need to use a fold
+    let totalcalories = top3calories.iter().fold(0, |acc, x| acc + x.1);
+    println!("The top three elves have a total of {:?} calories", totalcalories);
 }
 
-fn findMostCalories(input: Vec<(String, Vec<i32>)>) -> (String, i32) {
+// for part 1
+fn find_most_calories(input: &Vec<(String, Vec<i32>)>) -> (String, i32) {
     let mut most_calories = 0;
     let mut most_calories_elf = String::new();
-    for i in &input {
+    input.iter().for_each(|i| {
         let mut calories = 0;
         for j in &i.1 {
             calories += j;
@@ -57,6 +64,22 @@ fn findMostCalories(input: Vec<(String, Vec<i32>)>) -> (String, i32) {
             most_calories = calories;
             most_calories_elf = i.0.clone();
         }
-    }
+    });
     (most_calories_elf, most_calories)
+}
+
+// for part2
+fn top_three_calorie_elfs(input: &Vec<(String, Vec<i32>)>) -> Vec<(String, i32)> {
+    let mut top_three_calorie_elfs = Vec::new(); //new vector array
+    input.iter().for_each(|i| {
+        // for every elf in the passed in vector,
+        let mut calories = 0; // have a caloric counter
+        for j in &i.1 {
+            calories += j;
+        }
+        top_three_calorie_elfs.push((i.0.clone(), calories)); // add the elf and their calories to the vector, to keep track of each of them
+    });
+    top_three_calorie_elfs.sort_by(|a, b| b.1.cmp(&a.1)); // sort the vector by the calories
+    top_three_calorie_elfs.truncate(3); // truncate the vector to only the top 3
+    top_three_calorie_elfs // return the vector
 }
