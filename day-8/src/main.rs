1e24b2fd2fc8c0906a3e902b45e40ef9f4b15f98
mylo <milopadma@yahoo.com>
2022-12-09 21:59:57 +0800

    better

--- day-8/src/main.rs
+++ day-8/src/main.rs
@@ -22,7 +22,7 @@ fn main() {
         }
         tree.push(row);
     }
-    print!("Input is: {:?}", tree);
+    println!("Input is: {:?}", tree);
 
     // at this point, the input is a 2D vector of numbers
     // we can now iterate over the vector and count the visible trees
@@ -32,67 +32,249 @@ fn main() {
     // All of the trees around the edge of the grid are visible - since they are already on the edge,
     // trees are only visible if the trees between them and the edge are shorter.
 
-    let mut visible_trees = 0;
-    // vector to keep track the trees that are visible
-    let mut visible: Vec<u32> = Vec::new();
+    // lets count the total number of trees first
+    let total_trees = count_total_trees(&tree);
+    println!("Total trees: {}", total_trees);
 
+    // now lets count the number of trees on the edges
+    let edge_trees = count_edge_trees(&tree);
+    println!("Edge trees: {}", edge_trees);
+
+    // now lets count the number of visible trees
+    let total_visible = count_visible_numbers(&tree);
+    println!("Visible trees: {}", total_visible);
+
+    // for part 2, need to count the "scenic score"
+    let all_tree_scenic_score = count_scenic_score(&tree);
+    // print out the vector after each element has been multiplied by its 4 cardinal directions
+    println!(
+        "Scenic scores: {:?}",
+        all_tree_scenic_score.tree_scenic_scores
+            .iter()
+            .map(|x| x.north * x.south * x.east * x.west)
+            .collect::<Vec<u32>>()
+    );
+
+    println!(
+        "Highest scenic score: {}",
+        all_tree_scenic_score.tree_scenic_scores
+            .iter()
+            .max_by_key(|x| x.north + x.south + x.east + x.west)
+            .unwrap().north +
+            all_tree_scenic_score.tree_scenic_scores
+                .iter()
+                .max_by_key(|x| x.north + x.south + x.east + x.west)
+                .unwrap().south +
+            all_tree_scenic_score.tree_scenic_scores
+                .iter()
+                .max_by_key(|x| x.north + x.south + x.east + x.west)
+                .unwrap().east +
+            all_tree_scenic_score.tree_scenic_scores
+                .iter()
+                .max_by_key(|x| x.north + x.south + x.east + x.west)
+                .unwrap().west
+    );
+    println!("Scenic scores: {:?}", all_tree_scenic_score);
+    // 1. 2765952 is too high
+    // 2. 1420978 is too high
+    // 3. 145 is too low
+}
+
+#[derive(Debug)]
+struct TreeScenicScore {
+    tree_height: u32,
+    north: u32,
+    south: u32,
+    east: u32,
+    west: u32,
+}
+
+#[derive(Debug)]
+struct AllTreeScenicScores {
+    tree_height: Vec<u32>,
+    tree_scenic_scores: Vec<TreeScenicScore>,
+}
+
+fn count_scenic_score(tree: &Vec<Vec<u32>>) -> AllTreeScenicScores {
+    // init new struct of TreeScenicScore
+    let mut tree_scenic_score = TreeScenicScore {
+        tree_height: 0,
+        north: 0,
+        south: 0,
+        east: 0,
+        west: 0,
+    };
+
+    // init new struct of AllTreeScenicScores
+    let mut all_tree_scenic_scores = AllTreeScenicScores {
+        tree_height: Vec::new(),
+        tree_scenic_scores: Vec::new(),
+    };
+
+    // iterate through the matrix
     for row in 0..tree.len() {
-        for col in 0..tree[row].len() {
-            // for the edge checks
-            if row == 0 || row == tree.len() - 1 || col == 0 || col == tree[row].len() - 1 {
-                visible.push(tree[row][col]);
-                visible_trees += 1;
-                // skip the rest of the checks
-                continue;
+        for column in 0..tree[row].len() {
+            let current_number = tree[row][column];
+
+            // Find the coordinates of the number in the matrix
+            let mut coords = (0, 0);
+            for i in 0..tree.len() {
+                for j in 0..tree[i].len() {
+                    if tree[i][j] == current_number {
+                        coords = (i, j);
+                        break;
+                    }
+                }
             }
 
-            // for the cardinal direction checks
-            let mut visible_from_left = true;
-            let mut visible_from_right = true;
-            let mut visible_from_top = true;
-            let mut visible_from_bottom = true;
-            // check if the tree is visible from the left
-            for i in 0..col {
-                if tree[row][i] >= tree[row][col] {
-                    visible_from_left = false;
+            // Measure the viewing distance by looking up
+            for i in (0..coords.0).rev() {
+                if tree[i][coords.1] == current_number || tree[i][coords.1] < current_number {
                     break;
                 }
+                tree_scenic_score.north += 1;
             }
-            // check if the tree is visible from the right
-            for i in col + 1..tree[row].len() {
-                if tree[row][i] >= tree[row][col] {
-                    visible_from_right = false;
-                    break;
-                }
-            }
-            // check if the tree is visible from the top
-            for i in 0..row {
-                if tree[i][col] >= tree[row][col] {
-                    visible_from_top = false;
-                    break;
-                }
-            }
-            // check if the tree is visible from the bottom
-            for i in row + 1..tree.len() {
-                if tree[i][col] >= tree[row][col] {
-                    visible_from_bottom = false;
-                    break;
-                }
-            }
-            // a tree is only not visible if it is not visible from all directions
-            if visible_from_left && visible_from_right && visible_from_top && visible_from_bottom {
-                // add it to the vector
-                visible.push(tree[row][col]);
 
-                visible_trees += 1;
+            // then to the same for the other 3 cardinal directions
+            // check down
+            for k in row + 1..tree.len() {
+                if tree[k][column] >= tree[row][column] {
+                    tree_scenic_score.south += 1;
+                    break;
+                } else {
+                    tree_scenic_score.south += 1;
+                }
+            }
+
+            // check left
+            for k in 0..column {
+                if tree[row][k] >= tree[row][column] {
+                    tree_scenic_score.east += 1;
+                    break;
+                } else {
+                    tree_scenic_score.east += 1;
+                }
+            }
+
+            // check right
+            for k in column + 1..tree[row].len() {
+                if tree[row][k] >= tree[row][column] {
+                    tree_scenic_score.west += 1;
+                    break;
+                } else {
+                    tree_scenic_score.west += 1;
+                }
+            }
+
+            tree_scenic_score.tree_height = tree[row][column];
+
+            // add the total to the vector
+            all_tree_scenic_scores.tree_scenic_scores.push(tree_scenic_score);
+            // add the tree height
+            all_tree_scenic_scores.tree_height.push(tree[row][column]);
+            // and reset the tree_scenic_score
+            tree_scenic_score = TreeScenicScore {
+                tree_height: 0,
+                north: 0,
+                south: 0,
+                east: 0,
+                west: 0,
+            };
+        }
+    }
+
+    // return the vector
+    all_tree_scenic_scores
+}
+
+fn count_visible_numbers(tree: &Vec<Vec<u32>>) -> u32 {
+    let mut count = 0;
+
+    // Go through each element in the matrix
+    for row in 0..tree.len() {
+        for column in 0..tree[row].len() {
+            // Check if the current element is visible by looking up
+            let mut visible = true;
+            for k in 0..row {
+                if tree[k][column] >= tree[row][column] {
+                    visible = false;
+                    break;
+                }
+            }
+
+            // If the current element is not visible by looking up,
+            // check if it is visible by looking down
+            if !visible {
+                visible = true;
+                for k in row + 1..tree.len() {
+                    if tree[k][column] >= tree[row][column] {
+                        visible = false;
+                        break;
+                    }
+                }
+            }
+
+            // If the current element is not visible by looking up or down,
+            // check if it is visible by looking left
+            if !visible {
+                visible = true;
+                for k in 0..column {
+                    if tree[row][k] >= tree[row][column] {
+                        visible = false;
+                        break;
+                    }
+                }
+            }
+
+            // If the current element is not visible by looking up, down, or left,
+            // check if it is visible by looking right
+            if !visible {
+                visible = true;
+                for k in column + 1..tree[row].len() {
+                    if tree[row][k] >= tree[row][column] {
+                        visible = false;
+                        break;
+                    }
+                }
+            }
+
+            // If the current element is visible, increment the count
+            if visible {
+                count += 1;
             }
         }
     }
 
-    println!("Visible trees: {}", visible_trees);
-    println!("Visible trees: {:?}", visible);
+    count
+}
+
+fn count_edge_trees(tree: &Vec<Vec<u32>>) -> u32 {
+    let mut edge_trees = 0;
+    // make sure not to count the trees that are on the edges twice, by offsetting the loop by 1, respectively
+    // or.. just count how many entries are on the first and last rows
+    for row in 0..tree.len() {
+        for col in 0..tree[row].len() {
+            if row == 0 || row == tree.len() - 1 || col == 0 || col == tree[row].len() - 1 {
+                edge_trees += 1;
+            }
+        }
+    }
+    edge_trees
+}
+
+fn count_total_trees(tree: &Vec<Vec<u32>>) -> u32 {
+    let mut total_trees = 0;
+    for row in 0..tree.len() {
+        for col in 0..tree[row].len() {
+            if tree[row][col] != 0 {
+                total_trees += 1;
+            }
+        }
+    }
+    total_trees
 }
 // 1. 831, too low
 // 2. 731, too low
 // 3. 827, too low
 // 4. 9272, incorrect
+// 5. 1693, correct
