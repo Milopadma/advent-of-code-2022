14fc152e79c2687b2a4420baa5a4fc9ac1824ca8
mylo <milopadma@yahoo.com>
2022-12-09 14:13:56 +0800

    slowly..

--- day-8/target/debug/deps/day_8-a0a7f79e5749eb00.d
+++ day-8/target/debug/deps/day_8-a0a7f79e5749eb00.d
@@ -1,5 +1,6 @@
-/Users/mylo/Code/adventofcode-2022/day-8/target/debug/deps/day_8-a0a7f79e5749eb00.rmeta: src/main.rs
+/Users/mylo/Code/adventofcode-2022/day-8/target/debug/deps/day_8-a0a7f79e5749eb00.rmeta: src/main.rs src/test_input.txt
 
-/Users/mylo/Code/adventofcode-2022/day-8/target/debug/deps/day_8-a0a7f79e5749eb00.d: src/main.rs
+/Users/mylo/Code/adventofcode-2022/day-8/target/debug/deps/day_8-a0a7f79e5749eb00.d: src/main.rs src/test_input.txt
 
 src/main.rs:
+src/test_input.txt:
