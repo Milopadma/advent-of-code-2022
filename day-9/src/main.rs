d263ade98e1086678ea6bd4c92fa7dac5a6c20b2
mylo <milopadma@yahoo.com>
2022-12-15 19:06:06 +0800

    im lost

--- day-9/src/main.rs
+++ day-9/src/main.rs
@@ -36,11 +36,13 @@ fn main() {
 
     // move the head and tail according to the input
     let mut first_move = true;
+
     for line in split_input {
         let (direction, distance) = line.split_at(1);
         // since the tail follows the head, the tail will always be one step behind the head
         // offset the tail by one step in the direction of the head
         println!("Direction: {}, Distance: {}", direction, distance);
+
         let mut distance = distance.trim().parse::<isize>().unwrap();
 
         // now move the head and tail normally according to the input
@@ -54,33 +56,33 @@ fn main() {
                 _ => panic!("Invalid direction"),
             };
 
-            // update the grid
-            grid[head.0 as usize][head.1 as usize] += 1;
+            // only start updating the tails location after the head has moved once
+            // After each step, you'll need to update the position of the tail if the step means the head is no longer adjacent to the tail
+            // Check if the head is two steps away from the tail in any direction
+            if !first_move {
+                if (head.0 - tail.0).abs() > 1 || (head.1 - tail.1).abs() > 1 {
+                    // move the tail in the direction specified
+                    tail = match direction {
+                        "U" => (tail.0 - 1, tail.1),
+                        "D" => (tail.0 + 1, tail.1),
+                        "L" => (tail.0, tail.1 - 1),
+                        "R" => (tail.0, tail.1 + 1),
+                        _ => panic!("Invalid direction"),
+                    };
 
-            distance -= 1;
-        }
-        // only start updating the tails location after the head has moved once
-        // After each step, you'll need to update the position of the tail if the step means the head is no longer adjacent to the tail
-        // Check if the head is two steps away from the tail in any direction
-
-        if (head.0 - tail.0).abs() == 2 || (head.1 - tail.1).abs() == 2 {
-            // Move the tail in the same direction as the head
-            tail = match direction {
-                "U" => (tail.0 - 1, tail.1),
-                "D" => (tail.0 + 1, tail.1),
-                "L" => (tail.0, tail.1 - 1),
-                "R" => (tail.0, tail.1 + 1),
-                _ => panic!("Invalid direction"),
-            };
-        } else if (head.0 - tail.0).abs() == 1 && (head.1 - tail.1).abs() == 1 {
-            // Move the tail in the same direction as the head
-            tail = match direction {
-                "U" => (tail.0 - 1, tail.1),
-                "D" => (tail.0 + 1, tail.1),
-                "L" => (tail.0, tail.1 - 1),
-                "R" => (tail.0, tail.1 + 1),
-                _ => panic!("Invalid direction"),
-            };
+                    // update the grid
+                    grid[tail.0 as usize][tail.1 as usize] += 1;
+                } else if
+                    // this case we move the tail if its adjacent to the head
+                    (head.0 - tail.0).abs() == 1 ||
+                    (head.1 - tail.1).abs() == 1
+                {
+                    // update the grid
+                    grid[tail.0 as usize][tail.1 as usize] += 1;
+                }
+            } else {
+                first_move = false;
+            }
         }
 
         // print the grid
